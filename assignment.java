790518e309d57ad6b1431037c0d3ed15a160b46e
tuannndt <tuann643123@gmail.com>
2024-08-21 21:56:50 +0700

    Add files via upload

---
+++ assignment.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class assignment {
+
+    public static void main(String[] args) {
+        Scanner  sc = new Scanner(System.in);
+        System.out.print("Nhap a:");
+        float a = sc.nextFloat();
+        System.out.print("Nhap b:");
+        float b = sc.nextFloat();
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }  
+}
